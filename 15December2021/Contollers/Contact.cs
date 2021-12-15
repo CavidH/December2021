a9a57df3a34ea55edbd170ae7adc7caa17f10b1e
tu201906038 <tu201906038@215-7>
2021-12-15 19:20:55 +0400

    Add project files.

---
+++ 15December2021/Contollers/Contact.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _15December2021.Contollers
+{
+    public class Contact : Controller
+    {
+        // GET
+        public IActionResult Index()
+        {
+            return View();  
+        }
+    }
+}
---
+++ 15December2021/Contollers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _15December2021.Contollers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            //ViewBag.name = "Cavid";
+            //ViewBag.surname = "Haciyev";
+            return View();
+        }
+    }
+}
